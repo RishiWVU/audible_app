4cb1d9ef7df16be3977d543e30ad5cee3c4ba35e
Rishi <rreddy@mix.wvu.edu>
2021-10-19 11:42:47 -0400

    Authentication Service

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,13 +1,27 @@
 import { Component } from "@angular/core";
 import { NgForm } from "@angular/forms";
+import { AuthService } from "./auth.service";
+import { AuthResponse } from "./authResponse";
 
 @Component({
     selector: "audible-auth",
     templateUrl: "auth.component.html"
 })
 export class AuthComponent {
+    constructor(private authService:AuthService){
+    }
+    
     public onSubmit(data: NgForm) {
         console.log("Button clicked");
         console.log(data);
+        this.authService.signup(data.value.email, data.value.password).subscribe(
+            (data:AuthResponse) => {
+                console.log(data)
+            },
+            error => {
+                console.log(error.error)
+            }
+        );
+
     }
 }
