01b7ffe82efe1c30ca5fbd4daaa6e9728a0bf77a
Rishi <rreddy@mix.wvu.edu>
2021-10-19 10:56:38 -0400

    authentication component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { HelpPageComponent } from './body/help-page.component';
 import { MemberPageComponent } from './body/member-page.component';
 import { SearchPageComponent } from './body/search-page.component';
 import { SignInPageComponent } from './body/signin-page.component';
+import { AuthComponent } from './auth/auth.component';
 
 const routes: Routes= [
   {path: 'home', component: HomePageComponent},
@@ -14,6 +15,7 @@ const routes: Routes= [
   {path: 'member-page', component: MemberPageComponent},
   {path: 'search-page', component: SearchPageComponent},
   {path: 'signin-page', component: SignInPageComponent},
+  {path: 'auth', component: AuthComponent},
   {path: '', redirectTo: '/home', pathMatch:'full'}
 ]
 
